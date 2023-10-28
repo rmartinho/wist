d33673ea1e6d8226aecfd0f1b02c22241b3fc05b
R. Martinho Fernandes <rmf@rmf.io>
2023-10-28 11:41:16 +0200

    Make add card not insert invalid cards in list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="list">
-    <button @click="cards.unshift(makeCard())">
-      <add-icon />
-    </button>
+    <div class="buttons">
+      <button @click="onAddCard">
+        <add-icon />
+      </button>
+      <button>
+        <filter-icon />
+      </button>
+    </div>
+    <div v-if="newCard">
+      <card-list-item v-model="newCard" mode="edit" @save="onNewSave" @remove="onNewRemove" />
+    </div>
+    <div v-if="filter">
+    </div>
     <card-list v-model="cards" mode="edit" @save="onChange" @remove="onChange" />
   </div>
 </template>
@@ -13,6 +23,8 @@ import { parseJsonFromStorage, writeJsonToStorage } from '@/utils/storage'
 import { makeCard, cardIsValid, type Card } from '@/card'
 import CardList from '@/components/cards/CardList.vue'
 import AddIcon from '@/assets/images/add.svg?component'
+import FilterIcon from '@/assets/images/search.svg?component'
+import CardListItem from '@/components/cards/CardListItem.vue'
 
 const storageKey = 'cards'
 
@@ -31,6 +43,23 @@ const cards = ref(load())
 function onChange() {
   writeJsonToStorage(window.localStorage, storageKey, cards.value)
 }
+
+const newCard = ref(undefined as Card | undefined)
+
+function onAddCard() {
+  newCard.value = makeCard()
+}
+
+function onNewSave() {
+  cards.value.unshift(newCard.value!)
+  newCard.value = undefined
+}
+
+function onNewRemove() {
+  newCard.value = undefined
+}
+
+const filter = ref(undefined)
 </script>
 
 <style scoped>
@@ -40,8 +69,13 @@ function onChange() {
   flex-flow: column;
 }
 
-button {
+.buttons {
   margin-left: 4px;
+  display: flex;
+  flex-flow: row;
+}
+
+button {
   width: 24px;
 }
 </style>
